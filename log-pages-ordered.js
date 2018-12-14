09285e7c6a286340ee1ef5f5e1adbdeefafc1642
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2018-12-14 16:45:34 -0500

    Create log-pages-ordered.js

---
+++ log-pages-ordered.js
@@ -0,0 +1,26 @@
+const data = {
+	blank: 2,
+	first: 1,
+	last: 267
+}
+
+const pagesUnordered = [];
+
+for (let k = data.first; k <= data.last; ++k) {
+	pagesUnordered.push(k);
+}
+
+pagesUnordered.splice(pagesUnordered.length - 1, 0, data.blank);
+
+console.log(pagesUnordered);
+
+const pagesOrdered = [];
+
+for (let k = (pagesUnordered.length - 1), l = 0; k > l; k -= 4, l += 4) {
+	[0, 2, 1, 3].forEach((el, m, arr) => {
+		pagesOrdered.push(pagesUnordered[k - el]);
+		pagesOrdered.push(pagesUnordered[l + el]);
+	});
+}
+
+console.log(pagesOrdered);
